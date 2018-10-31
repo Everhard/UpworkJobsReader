aeb32d564e04cec835f96ca0394a0700497cdd25
Andrew Dorokhov <andrew@dorohoff.net>
2018-10-31 20:20:47 +0200

    Add UpworkJob class.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 [![Total Downloads](https://poser.pugx.org/steadfast/upwork-jobs-reader/downloads)](https://packagist.org/packages/steadfast/upwork-jobs-reader)
 [![License](https://poser.pugx.org/steadfast/upwork-jobs-reader/license)](https://packagist.org/packages/steadfast/upwork-jobs-reader)
 
-Upwork Jobs Reader.
+Upwork Jobs Reader gives the ability to get the last 30 jobs from Upwork RSS feed.
 
 ## Installation
 ```composer log
@@ -17,6 +17,35 @@ $reader = UpworkJobsReader;
 $jobs = $reader->fetchJobs();
 
 foreach($jobs as $job) {
-  echo $job['title'];
+  echo $job->title;
+  echo $job->link;
+  echo $job->description;
+  echo $job->created_date;
+  echo $job->posted_date;
+  echo $job->category
+  echo $job->country;
+  echo $job->skills;
+  echo $job->budget;
 }
+```
+
+## Filters
+Sort by newest jobs:
+``` php
+$reader->setSortBy(UpworkJobsReader::SORT_NEWEST);
+```
+
+Sort by relevance jobs:
+``` php
+$reader->setSortBy(UpworkJobsReader::SORT_RELEVANCE);
+```
+
+Sort by client money spending:
+``` php
+$reader->setSortBy(UpworkJobsReader::SORT_CLIENT_SPENDING);
+```
+
+Sort by client rating:
+``` php
+$reader->setSortBy(UpworkJobsReader::SORT_CLIENT_RATING);
 ```
--- src/UpworkJobsReader.php
+++ src/UpworkJobsReader.php
@@ -8,11 +8,6 @@ class UpworkJobsReader
     const JOB_TYPE_HOURLY   = 'hourly';
     const JOB_TYPE_FIXED    = 'fixed';
 
-    const EXPERIENCE_LEVEL_ALL          = 0;
-    const EXPERIENCE_LEVEL_ENTRY        = 1;
-    const EXPERIENCE_LEVEL_INTERMEDIATE = 2;
-    const EXPERIENCE_LEVEL_EXPERT       = 3;
-
     const SORT_NEWEST           = 'renew_time_int';
     const SORT_RELEVANCE        = 'relevance';
     const SORT_CLIENT_SPENDING  = 'client_total_charge';
@@ -21,9 +16,11 @@ class UpworkJobsReader
     /*
      * Default values:
      */
-    private $job_type           = self::JOB_TYPE_ALL;
-    private $experience_level   = self::EXPERIENCE_LEVEL_ALL;
-    private $sort               = self::SORT_NEWEST;
+    private $job_type                       = self::JOB_TYPE_ALL;
+    private $experience_entry_level         = false;
+    private $experience_intermediate_level  = false;
+    private $experience_expert_level        = false;
+    private $sort_by                        = self::SORT_NEWEST;
 
     public function __construct($options = [])
     {
@@ -34,15 +31,45 @@ class UpworkJobsReader
     {
         $jobs = [];
 
-        $feed = Feed::loadRss(self::RSS_FEED_URL);
+        $feed = Feed::loadRss($this->getCompiledUrl());
 
         foreach ($feed->item as $item) {
-            $jobs[] = [
-                'title' => (string) $item->title,
-            ];
+            $jobs[] = UpworkJob::fetchFromXml($item);
         }
 
         return $jobs;
+    }
+
+    public function setJobType($job_type)
+    {
+        $this->job_type = $job_type;
+    }
+
+    public function setSortBy($sort_by)
+    {
+        $this->sort_by = $sort_by;
+    }
+
+    public function setExperienceEntry($status)
+    {
+        $this->experience_entry_level = $status;
+    }
+
+    public function setExperienceIntermediate($status)
+    {
+        $this->experience_intermediate_level = $status;
+    }
+
+    public function setExperienceExpert($status)
+    {
+        $this->experience_expert_level = $status;
+    }
+
+    public function clearExperienceFilter()
+    {
+        $this->experience_entry_level           = false;
+        $this->experience_intermediate_level    = false;
+        $this->experience_expert_level          = false;
     }
 
     private function setOptions($options = [])
@@ -56,6 +83,53 @@ class UpworkJobsReader
 
     private function getCompiledUrl()
     {
-        return self::RSS_FEED_URL;
+        $request_url = self::RSS_FEED_URL;
+
+        /*
+         * Sorting:
+         */
+        $request_url .= "?" . "sort=" . $this->sort_by . "+desc";
+
+        /*
+         * Job type:
+         */
+        if ($this->job_type != self::JOB_TYPE_ALL) {
+            $request_url .= "&job_type=" . $this->job_type;
+        }
+
+        /*
+         * Experience level:
+         */
+        if ($this->experience_entry_level || $this->experience_intermediate_level || $this->experience_expert_level) {
+
+            if (!($this->experience_entry_level && $this->experience_intermediate_level && $this->experience_expert_level)) {
+
+                $request_url .= "&contractor_tier=";
+
+                $option_selected = false;
+
+                if ($this->experience_entry_level) {
+                    $request_url .= "1";
+                    $option_selected = true;
+                }
+
+                if ($this->experience_intermediate_level) {
+                    if ($option_selected) {
+                        $request_url .= ",";
+                    }
+                    $request_url .= "2";
+                    $option_selected = true;
+                }
+
+                if ($this->experience_expert_level) {
+                    if ($option_selected) {
+                        $request_url .= ",";
+                    }
+                    $request_url .= "3";
+                }
+            }
+        }
+
+        return $request_url;
     }
 }
